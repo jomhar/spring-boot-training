7c1356a40c9f8938783158407b77c976499a7f5c
jomhar <jomharpogs@gmail.com>
2018-10-29 10:57:48 +0000

    commit 3

--- training/src/main/java/com/tcs/training/controller/HomeController.java
+++ training/src/main/java/com/tcs/training/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     
-    @GetMapping("/")
+    @GetMapping("/default")
     public String home(){
         return "home";
     }
